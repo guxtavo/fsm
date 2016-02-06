22886cc833562f805dd02be2e13963c970d9fcd4
guxtavo <guxtavo@gmail.com>
2016-02-06 10:16:39 -0200

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#http://www.minecraftforum.net/forums/support/server-support/server-administration/1891264-tut
-orial-on-running-minecraft-on-a-dedicated
-export JAVA=~/jre1.8.0_71/bin/java
-export JAR=~/minecraft_server.1.8.9.jar
+#http://www.minecraftforum.net/forums/support/server-support/server-administration/1891264-tutorial-on-running-minecraft-on-a-dedicated
+export JAVA=~/fsm/jre1.8.0_71/bin/java
+export JAR=~/fsm/minecraft_server.1.8.9.jar
 nohup $JAVA -Xmx1024M -Xms768M -jar $JAR nogui &
