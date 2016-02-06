bfedc8539ec27ba0e20138ec598bccca9c64661b
guxtavo <guxtavo@gmail.com>
2016-02-06 10:17:26 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget http://javadl.sun.com/webapps/download/AutoDL?BundleId=114681
 wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.9/minecraft_server.1.8.9.jar
+tar xf AutoDL?BundleId=114681
